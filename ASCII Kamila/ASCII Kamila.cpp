2afa73d35efcfed6b5f5189063ff442f7f09bc83
kamiaceves <kami.acevesdlv29@gmail.com>
2020-03-11 23:04:23 -0600

    first commit

---
+++ ASCII Kamila/ASCII Kamila.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+int main() {
+
+	std::cout << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << "TABLA ASCII KAMILA" << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << "\n";
+	for (int i = 33; i < 128; i++) {
+
+		std::cout << (char)186 << "\t" << i << ":" << (char)i << "\t" << (char)186;
+		std::cout << (char)186 << "\t" << (i + 128) << ":" << (char)(i + 128) << "\t" << (char)186 << "\n";
+
+
+
+	}
+	std::cout << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205;
+	return 0;
+} //Pongame 100 profeeee <3 
